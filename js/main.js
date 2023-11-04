05cfb59a7c7094def784fe7e5a99f05d1d27cfa9
linuxuser94 <dzebo.emir@gmail.com>
2023-11-04 19:39:42 +0100

    javascript arrays are hard man

--- js/main.js
+++ js/main.js
@@ -4,18 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
     let p_all;
     let turn_count = 0;
 
-    p1 = document.getElementById("p1");
-    p2 = document.getElementById("p2");
-    p3 = document.getElementById("p3");
-
-    p4 = document.getElementById("p4");
-    p5 = document.getElementById("p5");
-    p6 = document.getElementById("p6");
-
-    p7 = document.getElementById("p7");
-    p8 = document.getElementById("p8");
-    p9 = document.getElementById("p9");
-
     let minValue = 0;
     let maxValue = 100;
     let player_turn_game;
@@ -40,17 +28,36 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("click", function() {
             if (player_turn_game === 1){
             button.textContent="X"
+            button.style.color='blue';
             player_turn_game = 0
             turn_count = turn_count + 1
             console.log("Turn count: ", turn_count)
             document.getElementById("player_turn").innerHTML = "OKS je na redu";
+            button.disabled = true;
         } else {
             button.textContent="O"
+            button.style.color='red';
             player_turn_game = 1
             turn_count = turn_count + 1
             console.log("Turn count: ", turn_count)
             document.getElementById("player_turn").innerHTML = "X je na redu";
+            button.disabled = true;
         }         
         });
     });
+
+    let p_combo_win = [
+        p1 = document.getElementById("p1"),
+        p2 = document.getElementById("p2"),
+        p3 = document.getElementById("p3"),
+
+        p4 = document.getElementById("p4"),
+        p5 = document.getElementById("p5"),
+        p6 = document.getElementById("p6"),
+
+        p7 = document.getElementById("p7"),
+        p8 = document.getElementById("p8"),
+        p9 = document.getElementById("p9"),
+    ]
+
 });
